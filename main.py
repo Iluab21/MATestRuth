a16cb9c951c33106bf1ad20fc145cb83d84eeeab
Timofey <timofejfomin99@gmail.com>
2026-02-15 17:25:17 +0300

    fourth commit

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 import instructions
 from kivy.uix.textinput import TextInput
+from pydantic import BaseModel, Field, ValidationError
+from typing import Optional, Literal
+
+
 class MyApp(App):
     def build(self):
         sm = ScreenManager()
@@ -19,38 +23,48 @@ class FirstScreen(Screen):
         super().__init__(**kwargs)
         layout = BoxLayout(orientation='vertical', spacing='10px', padding='10px')
         label = Label(text=instructions.txt_instruction)
-        textinput_name = TextInput(size_hint=(0.8, None), height='40px',
+        self.textinput_name = TextInput(size_hint=(0.8, None), height='40px',
                                    pos_hint={'center_x': 0.5}, hint_text='Имя')
-        textinput_age = TextInput(size_hint=(0.8, None), height='40px', pos_hint={'center_x': 0.5},
+        self.textinput_age = TextInput(size_hint=(0.8, None), height='40px', pos_hint={'center_x': 0.5},
                                   hint_text='Возраст')
         button = Button(text='Start', background_color='red',
                         size_hint=(0.8, None), height='40px', pos_hint={'center_x': 0.5})
         layout.add_widget(label)
-        layout.add_widget(textinput_name)
-        layout.add_widget(textinput_age)
+        layout.add_widget(self.textinput_name)
+        layout.add_widget(self.textinput_age)
         layout.add_widget(button)
         self.add_widget(layout)
         button.on_press = self.changing_screen
 
     def changing_screen(self):
-        self.manager.current = 'second screen'
+        age = self.textinput_age.text
+        name = self.textinput_name.text
+        try:
+            schema = SchemaFirstScreen(age=age, name=name)
+            self.manager.current = 'second screen'
+        except ValidationError:
+            self.textinput_age.text = ''
 class SecondScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         layout = BoxLayout(orientation='vertical', spacing='10px', padding='10px')
         label = Label(text=instructions.txt_test1)
-        textinput = TextInput(size_hint=(0.8, None), height='40px',
+        self.textinput = TextInput(size_hint=(0.8, None), height='40px',
                                    pos_hint={'center_x': 0.5}, hint_text='Введите результат')
         button = Button(text='Продолжить', background_color='green',
                         size_hint=(0.8, None), height='40px', pos_hint={'center_x': 0.5})
 
         layout.add_widget(label)
-        layout.add_widget(textinput)
+        layout.add_widget(self.textinput)
         layout.add_widget(button)
         self.add_widget(layout)
         button.on_press = self.changing_screen
     def changing_screen(self):
-        self.manager.current = 'third screen'
+        try:
+            schema = SchemaDimension(dimension=self.textinput.text)
+            self.manager.current = 'third screen'
+        except ValidationError:
+            self.textinput.text = ''
 class ThirdScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -69,18 +83,36 @@ class FourthScreen(Screen):
         super().__init__(**kwargs)
         layout = BoxLayout(orientation='vertical', spacing='10px', padding='10px')
         label = Label(text=instructions.txt_test3)
-        textinput_res = TextInput(size_hint=(0.8, None), height='40px',
+        self.textinput_res = TextInput(size_hint=(0.8, None), height='40px',
                                    pos_hint={'center_x': 0.5}, hint_text='Результат')
-        textinput_res2 = TextInput(size_hint=(0.8, None), height='40px', pos_hint={'center_x': 0.5},
+        self.textinput_res2 = TextInput(size_hint=(0.8, None), height='40px', pos_hint={'center_x': 0.5},
                                   hint_text='Результат после отдыха')
         button = Button(text='Завершить', background_color='green',
                         size_hint=(0.8, None), height='40px', pos_hint={'center_x': 0.5})
         layout.add_widget(label)
-        layout.add_widget(textinput_res)
-        layout.add_widget(textinput_res2)
+        layout.add_widget(self.textinput_res)
+        layout.add_widget(self.textinput_res2)
         layout.add_widget(button)
         self.add_widget(layout)
+        button.on_press = self.changing_screen
+    def changing_screen(self):
+        try:
+            schema = SchemaDimension(dimension=self.textinput_res2)
+        except ValidationError:
+            self.textinput_res.text = ''
+        try:
+            schema = SchemaDimension(dimension=self.textinput_res2)
+            self.manager.current = 'fifth screen'
+        except ValidationError:
+            self.textinput_res2.text = ''
 class FifthScreen(Screen):
     pass
+class SchemaFirstScreen(BaseModel):
+    age: Optional[int] = Field(..., ge=1, le=120)
+class SchemaDimension(BaseModel):
+    dimension: Optional[int] = Field(..., ge=1)
+
+
+
 app = MyApp()
 app.run()
